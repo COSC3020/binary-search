f23c36ca69606404fe54eba35baa927b3448d935
Lars Kotthoff <larsko@uwyo.edu>
2023-09-07 09:01:23 -0600

    fix test code

--- code.test.js
+++ code.test.js
@@ -6,6 +6,7 @@ eval(fs.readFileSync('code.js')+'');
 const testSearch =
     jsc.forall("array nat", function(arr) {
         if(arr.length > 0) {
+            arr.sort(function(a, b) { return a - b; });
             return binarySearch(arr, arr[0]) === 0;
         } else {
             return binarySearch(arr, "foo") === -1;
